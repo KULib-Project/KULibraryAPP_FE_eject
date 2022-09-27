1e6f8487727f8d314e452780f06a210b8845c9c7
VEA22 <76689078+VEA22@users.noreply.github.com>
2022-09-27 22:29:05 +0900

    fix: 작업내용 롤백

--- screens/PostDetail.js
+++ screens/PostDetail.js
@@ -33,58 +33,22 @@ export default function PostDetail({ navigation, route }) {
   const [ccontent, setcontent] = useState("");
   const [isLoding, setIsLoding] = useState(false);
 
-  const getDetail = () => {
-    const id = route.params.itemData.id;
-    return axios
+  useEffect(() => {
+    axios
       .get("https://ea6717d8-601b-42a2-b0e6-1c5f0ea9f1a1.mock.pstmn.io/id=1")
       .then((response) => {
         setComments(response.data);
         console.log("==========================");
-        console.log(
-          comments.comment.CComent.CcomentsList.map((c) => c.content)
-        );
+        comments.comment.commentsList.map((c) => {
+          console.log(c.content);
+        });
         console.log("++++++++++++++++++++++++++");
       });
+  }, []);
+
+  const getDetail = () => {
+    const id = route.params.itemData.id;
+    console.log(id);
   };
-  const PrintComment = () => {
-    // useQuery를 활용한 Data fetch
-    const query = useQuery(["allcomment"], getDetail);
-
-    // Print Error
-    if (query.isError) {
-      console.log(query.error.message);
-    }
-
-    // Print Loading Screen
-    if (query.isLoading) {
-      return (
-        <View>
-          <Text>Loading...</Text>
-        </View>
-      );
-    } else {
-      // Complete loading
-
-      return comments.comment.CComent.content.map((com) => (
-        // com.commentsList.map((c)=>(
-        //   <View>
-        //     <Text>ㅇ</Text>
-        //   </View>
-        // ))
-        <View>
-          <Text>{`${com}`}</Text>
-        </View>
-      ));
-    }
-  };
-
-  return (
-    <View>
-      <QueryClientProvider client={queryClient}>
-        <PrintComment />
-      </QueryClientProvider>
-      {getDetail()}
-      <Text>전성기..^^</Text>
-    </View>
-  );
+  return <View>{getDetail()}</View>;
 }
