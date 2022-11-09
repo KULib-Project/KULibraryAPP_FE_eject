0d8e26e25f6f78c04cd6fcb53b700194f3e044c6
VEA22 <cj9745@naver.com>
2022-11-09 20:34:03 +0900

    style: ESLint 적용

--- screens/Depart.js
+++ screens/Depart.js
@@ -1,49 +1,56 @@
-import React from 'react';
-import { FlatList,View,StyleSheet,ScrollView,Text,TouchableOpacity,StatusBar } from 'react-native';
+import React from "react";
+import {
+  FlatList,
+  View,
+  StyleSheet,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  StatusBar,
+} from "react-native";
 
-
-
-const data=['date']
-
+const data = ["date"];
 
 function Depart({ navigation }) {
   return (
     <View>
-        <StatusBar backgroundColor="transparent" barStyle="dark-content" />
-        <FlatList
-      inverted={-1}
-      data={data}
-      style={{display:'flex',flexDirection:'column'}}
-      renderItem={({item}) => ( 
-      <TouchableOpacity
-      onPress={() => navigation.navigate("Read Post")}
-      >
-          <View style={styles.postBox}>
-            <Text>제목</Text>
-            <Text>간단한 요약</Text>
-            <View style={styles.postSubBox}>
-              <Text>작성 시간 | 작성자</Text>
-              <View style={styles.subInfo}>
-                <Text>조횟수</Text>
-                <Text>댓글수</Text>
+      <StatusBar backgroundColor="transparent" barStyle="dark-content" />
+      <FlatList
+        inverted={-1}
+        data={data}
+        style={{ display: "flex", flexDirection: "column" }}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => navigation.navigate("Read Post")}>
+            <View style={styles.postBox}>
+              <Text>제목</Text>
+              <Text>간단한 요약</Text>
+              <View style={styles.postSubBox}>
+                <Text>작성 시간 | 작성자</Text>
+                <View style={styles.subInfo}>
+                  <Text>조횟수</Text>
+                  <Text>댓글수</Text>
+                </View>
               </View>
             </View>
-          </View>
-        </TouchableOpacity> )}/>
-      <TouchableOpacity 
-          activeOpacity={0.7}
-          onPress={() => navigation.navigate("Post")}
-          style={styles.touchableOpacityStyle}>
-            <View style={styles.postBtn}>
-              <Text>글쓰기</Text>
-            </View>
           </TouchableOpacity>
-          <TouchableOpacity 
-          activeOpacity={0.7}
-          onPress={() => alert('필터 버튼')}
-          style={styles.filterStyle}>
-              <Text>필터</Text>
-          </TouchableOpacity>
+        )}
+      />
+      <TouchableOpacity
+        activeOpacity={0.7}
+        onPress={() => navigation.navigate("PostDepart")}
+        style={styles.touchableOpacityStyle}
+      >
+        <View style={styles.postBtn}>
+          <Text>글쓰기</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        activeOpacity={0.7}
+        onPress={() => alert("필터 버튼")}
+        style={styles.filterStyle}
+      >
+        <Text>필터</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -94,18 +101,17 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     bottom: "-300%",
   },
-  filterStyle:{
-      borderWidth: 1,
-      borderColor: '#02A2AB',
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: 70,
-      position: 'absolute',
-      top: 590,
-      right: 20,
-      height: 70,
-      backgroundColor: '#02A2AB',
-      borderRadius: 100,
+  filterStyle: {
+    borderWidth: 1,
+    borderColor: "#02A2AB",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 70,
+    position: "absolute",
+    top: 590,
+    right: 20,
+    height: 70,
+    backgroundColor: "#02A2AB",
+    borderRadius: 100,
   },
-  
-})
+});
